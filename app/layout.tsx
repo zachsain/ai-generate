567ac73ad04a942d96eb25e9b91e4d55b3ca32cb
Zachary Sain <zachsain@gmail.com>
2023-09-01 10:30:37 -0500

    meta-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@ import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 import '../styles/globals.css';
 
-const title = 'Twitter Bio Generator';
-const description = 'Generate your next Twitter bio in seconds.';
+const title = 'ai-generator';
+const description = 'Generate great captions, lyrics, and pitches in seconds.';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://twitterbio.io'),
+  metadataBase: new URL('https://ai-generator.io'),
   title,
   description,
   openGraph: {
